851454bdf7d27ee68c154b59cb7ab8acd57ce76e
Никита <NVRudy@gmail.com>
2023-08-06 11:47:01 +0300

    HW8

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,72 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void task1() {
+        System.out.println("\n" + "Задача 1" + "\n");
+        int[] mass = new int[]{1, 2, 3};
+        double[] doouble = {1.57, 7.654, 9.986};
+        int[] pew = {2, 25, 87, 94, 100};
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task2() {
+        System.out.println("\n" + "Задача 2" + "\n");
+        int[] mass = {1, 2, 3};
+        for (int i = 0; i < mass.length; i++) {
+            if (i < mass.length - 1) {
+                System.out.print(mass[i] + ",");
+            } else {
+                System.out.println(mass[i]);
+            }
+        }
+        double[] doouble = {1.57, 7.654, 9.986};
+        System.out.println(doouble[2] + "," + doouble[1] + "," + doouble[0]);
+        int[] pew = {2, 25, 87, 94, 100};
+        for (int a = 0; a < pew.length; a++) {
+            if (a < pew.length - 1) {
+                System.out.print(pew[a] + ",");
+            } else {
+                System.out.println(pew[a]);
+            }
+        }
+    }
+
+
+    public static void task3() {
+        System.out.println("\n" + "Задача 3" + "\n");
+        int[] mass = {1, 2, 3};
+        for (int i = mass.length - 1; i >= 0; i--) {
+            if (i < 1) {
+                System.out.println(mass[i]);
+            } else {
+                System.out.print(mass[i] + ",");
+            }
+        }
+        double[] doouble = {1.57, 7.654, 9.986};
+        System.out.println(doouble[2] + "," + doouble[1] + "," + doouble[0]);
+        int[] pew = {2, 76, 98, 98787, 54, 34};
+        for (int i = pew.length - 1; i >= 0; i--) {
+            if (i < 1) {
+                System.out.print(pew[i]);
+            } else {
+                System.out.print(pew[i] + ",");
+            }
+        }
+    }
+
+    public static void task4() {
+        System.out.println("\n" + "Задача 4" + "\n");
+        int mass[] = new int[]{1, 2, 3};
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i]%2 == 1) {
+                mass[i] += 1;
+            }
+            System.out.print(mass[i] + " ");
         }
     }
 }
